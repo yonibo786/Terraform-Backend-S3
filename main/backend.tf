e350ed7b52e5de618cd588f16f757e3480dacbb5
yonibo786 <60070113+yonibo786@users.noreply.github.com>
2021-09-27 19:56:54 +0300

    Delete backend.tf

--- main/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-tantor"
-    region         = "eu-central-1"
-    dynamodb_table = "terraform-state-locking"
-    key            = "main/terraform-first.tfstate"
-  }
-}
