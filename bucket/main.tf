7ffe86416d6dba0612eea98f76021a7e4a623149
yonibo786 <60070113+yonibo786@users.noreply.github.com>
2021-09-27 19:22:05 +0300

    Update main.tf

--- bucket/main.tf
+++ bucket/main.tf
@@ -1,1 +1,17 @@
+provider "aws" {
+  region = "eu-central-1"
+}
 
+resource "aws_s3_bucket" "b" {
+  bucket = "${terraform.workspace}-tf-tantor"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+output "s3_bucket_id" {
+    value = aws_s3_bucket.b.s3_bucket.id
+}
