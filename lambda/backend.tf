d96d202a8d999adfe089e744b6ce824930fb16e8
yonibo786 <60070113+yonibo786@users.noreply.github.com>
2021-09-27 19:27:56 +0300

    Update backend.tf

--- lambda/backend.tf
+++ lambda/backend.tf
@@ -1,1 +1,8 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-tantor"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-state-locking"
+    key            = "main/terraform-first.tfstate"
+  }
+}
