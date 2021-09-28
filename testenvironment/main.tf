87eb138e117ecc5b024c8f321c7e59842cbf17c3
yonibo786 <60070113+yonibo786@users.noreply.github.com>
2021-09-28 17:44:25 +0300

    Update main.tf

--- testenvironment/main.tf
+++ testenvironment/main.tf
@@ -3,16 +3,22 @@ provider "aws" {
 }
 
 
-resource "aws_s3_bucket_object" "dist" {
-  for_each = fileset("./todo-list/", "*")
+# resource "aws_s3_bucket_object" "dist" {
+#   for_each = fileset("./todo-list/", "*")
 
-  bucket = "${terraform.workspace}-tantor-milions-of-files"
-  key    = each.value
-  source = "./todo-list/${each.value}"
-  etag   = filemd5("./todo-list/${each.value}")
-  acl    = "public-read"
-  depends_on = [aws_s3_bucket.b]
+#   bucket = "${terraform.workspace}-tantor-milions-of-files"
+#   key    = each.value
+#   source = "./todo-list/${each.value}"
+#   etag   = filemd5("./todo-list/${each.value}")
+#   acl    = "public-read"
+#   depends_on = [aws_s3_bucket.b]
 
+# }
+
+resource "null_resource" "remove_and_upload_to_s3" {
+  provisioner "local-exec" {
+    command = "aws s3 sync ./todo-list s3://${terraform.workspace}-tantor-milions-of-files"
+  }
 }
 
 
