172fad7bcf5b7d5f81d5e0be5ac33aa683340eca
yonibo786 <60070113+yonibo786@users.noreply.github.com>
2021-09-28 12:06:45 +0300

    Update main.tf

--- testenvironment/main.tf
+++ testenvironment/main.tf
@@ -2,12 +2,4 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "${terraform.workspace}-tf-tantor"
-  acl    = "private"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
