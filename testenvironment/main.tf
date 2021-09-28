4d4f47fde6620fdcf618f2e271ef25f9681d1f8c
yonibo786 <60070113+yonibo786@users.noreply.github.com>
2021-09-28 15:30:16 +0300

    Update main.tf

--- testenvironment/main.tf
+++ testenvironment/main.tf
@@ -10,7 +10,8 @@ resource "aws_s3_bucket_object" "dist" {
   key    = each.value
   source = "./todo-list/${each.value}"
   etag   = filemd5("./todo-list/${each.value}")
-  acl    = "public-read" 
+  acl    = "public-read"
+  content_type = each.value.content_type
   depends_on = [aws_s3_bucket.b]
 
 }
