04f581ee5e6e99348e6b73be3589cfa1df9d4d4d
yonibo786 <60070113+yonibo786@users.noreply.github.com>
2021-09-28 12:29:39 +0300

    Update main.tf

--- testenvironment/main.tf
+++ testenvironment/main.tf
@@ -3,3 +3,11 @@ provider "aws" {
 }
 
 
+resource "aws_s3_bucket_object" "dist" {
+  for_each = fileset("/home/pawan/Documents/Projects/", "*")
+
+  bucket = "test-terraform-tantor-milions-of-files"
+  key    = each.value
+  source = "/home/pawan/Documents/Projects/${each.value}"
+  etag   = filemd5("/home/pawan/Documents/Projects/${each.value}")
+}
