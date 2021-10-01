0078bca3745eddcc62168a08a3484824d57591d1
yonibo786 <60070113+yonibo786@users.noreply.github.com>
2021-10-01 11:19:41 +0300

    Update variables.tf

--- testenvironment/variables.tf
+++ testenvironment/variables.tf
@@ -1,2 +1,8 @@
-variable "region" {}
-variable "plan" {}
+variable "region" {
+  type        = string
+  description = "The region of the environment"
+}
+variable "plan" {
+  type        = string
+  description = "The features in the app determined by the plan"
+}
